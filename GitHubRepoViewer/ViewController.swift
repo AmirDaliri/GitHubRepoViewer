7c3a614b11d6aab4b48556d82adeede86281d7fa
Amir Daliri <amir.daliri.github@gmail.com>
2024-02-19 22:03:24 +0300

    Create Project

--- GitHubRepoViewer/ViewController.swift
+++ GitHubRepoViewer/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
 
